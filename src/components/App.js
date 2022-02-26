b5be8561a50e641cd9aa8d7cd5ecfa5b45cc4f1a
obviouslynottop1 <williamrichardson745@gmail.com>
2022-02-26 15:46:03 +0000

    init1

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import Signup from "./Signup";
+import React from "react"
+import { Container } from 'react-bootstrap'
+
+function App() {
+  return (
+    <Container
+    className="d-flex align-items-center justify-content-center"
+    style={{ minHeight: "100vh" }}>
+      <Signup />
+    </Container>
+  )
+}
+
+export default App;
